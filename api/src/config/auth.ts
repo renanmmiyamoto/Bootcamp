d92fbc3d6ac49c1fa35df70d285b64e02f7b3e99
Renan Miyamoto <renanmmiyamoto@gmail.com>
2020-06-24 15:56:12 -0300

    ending first step of api

---
+++ api/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: '99fa99020cd801726f51c5ac00ee0e7d',
+		expiresIn: '1d',
+	},
+};
--- api/src/routes/users.routes.ts
+++ api/src/routes/users.routes.ts
@@ -1,27 +1,46 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../service/CreateUserService';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
-	try {
-		const { name, email, password } = req.body;
+	const { name, email, password } = req.body;
 
-		const createUser = new CreateUserService();
+	const createUser = new CreateUserService();
 
-		const user = await createUser.execute({
-			name,
-			email,
-			password,
+	const user = await createUser.execute({
+		name,
+		email,
+		password,
+	});
+
+	delete user.password;
+
+	return res.json(user);
+});
+
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('avatar'),
+	async (req, res) => {
+		const updateUserAvatar = new UpdateUserAvatarService();
+
+		const user = await updateUserAvatar.execute({
+			user_id: req.user.id,
+			avatarFilename: req.file.filename,
 		});
 
 		delete user.password;
 
 		return res.json(user);
-	} catch (error) {
-		return res.status(400).json({ error: error.message });
-	}
-});
+	},
+);
 
 export default usersRouter;
